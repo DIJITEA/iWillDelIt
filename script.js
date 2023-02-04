02f0efde0dc25f28a7a6f95c32df0758e5be063e
DESKTOP-SHK9PBH\olegk <kornyakovo@gmail.com>
2023-02-04 04:57:39 +0400

    del

---
+++ script.js
@@ -0,0 +1,11 @@
+function bah() {
+  let piu = Math.floor(Math.random() * 5);
+  let luck = Math.floor(Math.random() * 5);
+  console.log("olo");
+
+  if (piu == luck) {
+    alert("u dead");
+  } else {
+    alert("lucky");
+  }
+}
